e7f63080d51d660c1f979a960f6f3e337963d10f
Tony Foster <anthonyfoster85@gmail.com>
2017-02-18 21:23:16 -0500

    Added jest debugging for VSCode

--- scripts/buildCss.js
+++ scripts/buildCss.js
@@ -2,14 +2,23 @@ var glob = require("glob")
 var path = require('path');
 var compileLess = require('./compileLess');
 
+if (process.argv.length < 3) {
+    console.log('Usage: node buildCss.js src dest');
+    return;
+}
+
+var src = path.join(process.cwd(), process.argv[2]);
+var dest = process.argv.length > 3 ? path.join(process.cwd(), process.argv[3]) : src; 
+
 const globOptions = {
-    cwd: path.join(process.cwd(), "src"),
-    absolute: true
+    cwd: src,
 };
 
 glob("**/*.less", globOptions, (er, files) => {
     if (files && files.length) {
-        files.forEach(f => compileLess(f));
+        files.forEach(f => {
+            compileLess(path.join(src, f), path.join(dest, f.substring(0, f.length - 4) + 'css'));
+        });
     }
 });
 
--- scripts/compileLess.js
+++ scripts/compileLess.js
@@ -1,19 +1,30 @@
 var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
 var less = require('less');
 
 const lessOptions = {};
 
-module.exports = function compileLess(file) {
-    fs.readFile(file, 'utf8', (err, data) => {
+module.exports = function compileLess(srcFile, destFile) {
+    fs.readFile(srcFile, 'utf8', (err, data) => {
         if (err) {
             return console.log(err);
         }
         less.render(data, lessOptions)
             .then((output) => {
-                var outfile = file.substring(0, file.length - 4) + 'css';
-                fs.writeFile(outfile, output.css, { flag : 'w' }, (err) => {
-                    if (err) throw err;
-                });  
+                mkdirp(path.dirname(destFile), (err) => {
+                    if (err) {
+                        throw err;
+                    }
+
+                    fs.writeFile(destFile, output.css, { flag : 'w' }, (err) => {
+                        if (err) {
+                            throw err;
+                        }
+                    });    
+                    
+                });
+                  
                 // output.css = string of css
                 // output.map = string of sourcemap
                 // output.imports = array of string filenames of the imports referenced
