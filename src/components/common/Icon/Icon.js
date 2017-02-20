7161c80cedda56944e9774f3ca058eeeb6dfbe1c
Tony Foster <anthonyfoster85@gmail.com>
2017-02-19 19:10:07 -0500

    Added SVG icon support, put in example

--- src/components/common/Icon/Icon.js
+++ src/components/common/Icon/Icon.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import './Icon.css';
+
+export const fontSize = ({ height }) => height ? `${height / 16}rem` : '1.0em'
 
 export const Icon = ({icon, ...props}) => {
-    const svg = require(`./generated/${icon}.svg.js`);
+    const iconStyle = Object.assign({}, props.style, {
+        fontSize: `${fontSize}`
+    });
+    const svg = require(`./assets/${icon}.svg.js`);
+    const className = 'icon' + (props.className ? ` ${props.className}` : '');
     return (
-        <span {...props} dangerouslySetInnerHTML={{ __html: svg }} />
+        <span {...props} className={className} style={iconStyle} dangerouslySetInnerHTML={{ __html: svg }} />
     );
 };
-
 
 Icon.propTypes = {
   icon: React.PropTypes.string.isRequired,
