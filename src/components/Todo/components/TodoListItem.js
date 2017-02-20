c536a2f8be908b24fbdf943c54ab3d067fe4e75b
Tony Foster <anthonyfoster85@gmail.com>
2017-02-19 19:18:51 -0500

    Fixes to icon components

--- src/components/Todo/components/TodoListItem.js
+++ src/components/Todo/components/TodoListItem.js
@@ -2,9 +2,6 @@ import React from 'react';
 import Icon from '../../common/Icon';
 import './TodoListItem.css';
 
-//  Inspirec by Arc: https://github.com/diegohaz/arc/blob/master/src/components/atoms/Icon/index.js
-export const fontSize = ({ height }) => height ? `${height / 16}rem` : '1.25em'
-
 export const TodoListItem = ({index, text, onDone, t}) => {
     return (
         <li className={'todo-list-item'}>
--- src/components/common/Icon/Icon.js
+++ src/components/common/Icon/Icon.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import './Icon.css';
 
-export const fontSize = ({ height }) => height ? `${height / 16}rem` : '1.0em'
-
 export const Icon = ({icon, ...props}) => {
-    const iconStyle = Object.assign({}, props.style, {
-        fontSize: `${fontSize}`
-    });
     const svg = require(`./assets/${icon}.svg.js`);
     const className = 'icon' + (props.className ? ` ${props.className}` : '');
     return (
-        <span {...props} className={className} style={iconStyle} dangerouslySetInnerHTML={{ __html: svg }} />
+        <span {...props} className={className} dangerouslySetInnerHTML={{ __html: svg }} />
     );
 };
 
